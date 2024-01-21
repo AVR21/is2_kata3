defa802b26291f8714cf3cf802e2310d6ea7c6dc
AVR21 <alejandroviera12@gmail.com>
2024-01-21 13:35:38 +0000

    First implementation

--- is2/ulpgc/kata2/Main.java
+++ is2/ulpgc/kata2/Main.java
@@ -1,5 +1,6 @@
 package is2.ulpgc.kata2;
 
+import is2.ulpgc.kata3.*;
 import java.util.List;
 import java.util.TreeMap;
 
@@ -11,5 +12,26 @@ public class Main {
         for(Double key: courses.keySet()){
             System.out.println(key + " : " + courses.get(key));
         }
+
+        MainJFrame main = new MainJFrame();
+        double[] values = courses.values().stream().mapToDouble(Integer::doubleValue).toArray();
+        main.histogramWindow().show(hist(values));
+        main.setVisible(true);
+
+    }
+
+    private static Histogram hist(double[] values) {
+        Histogram histogram = new Histogram() {
+            @Override
+            public int bins() {
+                return 6;
+            }
+
+            @Override
+            public double[] values() {
+                return values;
+            }
+        };
+        return histogram;
     }
 }
